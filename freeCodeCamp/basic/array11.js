ad6a84405c41e43f4ab448c25a32aeeecd6d05ee
Vyplasil Nicolas <nicolas.vyplasil@hotmail.com>
2023-06-09 11:18:49 +0400

    freeCodeCamp

---
+++ freeCodeCamp/basic/array11.js
@@ -0,0 +1,8 @@
+/**Shopping List
+Create a shopping list in the variable myList. The list should be a multi-dimensional
+array containing several sub-arrays.
+The first element in each sub-array should contain a string with the name of the item.
+The second element should be a number representing the quantity i.e.
+["Chocolate Bar", 15]
+There should be at least 5 sub-arrays in the list. */
+const myList = [ ["string1", 1], ["string2", 2], ["string3", 3], ["string4", 4], ["string5", 5]];
